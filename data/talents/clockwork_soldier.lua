75880e7d79ec93b7571bb7802e7e261f8a57ab9d
Darkgobbo1337 <74969469+Darkgobbo1337@users.noreply.github.com>
2021-08-19 12:38:15 -0500

    Update clockwork_soldier.lua

--- data/talents/clockwork_soldier.lua
+++ data/talents/clockwork_soldier.lua
@@ -6,12 +6,17 @@ newTalent{
 	mode = "passive",
 	getDamage = function(self, t) return 30 end,
 	getPercentInc = function(self, t) return math.sqrt(self:getTalentLevel(t) / 5) / 1.5 end,
+	getoffmult = function(self,t)
+		return	self:combatTalentLimit(t, 1, 0.65, 0.85)-- limit <100%
+		end,
 	info = function(self, t)
 		local damage = t.getDamage(self, t)
 		local inc = t.getPercentInc(self, t)
-		return ([[Increases weapon damage by %d%% and physical power by 30 when using swords, axes, maces, knives, or bows.
-		You now also use your Magic in place of Strength when equipping weapons and ammo as well as when calculating weapon damage.
-		These bonuses override rather than stack with weapon mastery, dagger mastery, and bow mastery.]]):
+		return ([[You wield the weapons of the clock, A two handed weapon in your main hand and a dagger in the off hand.
+		Increase weapon damage by 33-76% and Physical power by 30 when using swords, axes, maces, and knives. 
+		You now also use your Magic in place of Dexterity when equipping weapons as well as when calculating weapon damage.
+		You can wield a two handed weapon in your main hand and your off hand damage is increased to 65-85% 
+		These bonuses override rather than stack with weapon mastery and dagger mastery.]]):
 		tformat(100*inc)
 	end,
 }
